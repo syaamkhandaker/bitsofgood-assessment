d353be3019cff643db51aea4087b4613e35ea56a
Syaam Khandaker <syaamkhandaker@gmail.com>
2023-08-28 20:56:21 -0400

    updated counter to always add visits so it persists

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -22,10 +22,12 @@ export default function RoutesContainer() {
       }
       setUsers(response.data);
       const visits: any = {};
-      for (let x in response.data) {
-        visits[response.data[x].id] = 0;
+      if (!localStorage.getItem("visits")) {
+        for (let x in response.data) {
+          visits[response.data[x].id] = 0;
+        }
+        localStorage.setItem("visits", JSON.stringify(visits));
       }
-      localStorage.setItem("visits", JSON.stringify(visits));
       setLoading(false);
     };
     init();
